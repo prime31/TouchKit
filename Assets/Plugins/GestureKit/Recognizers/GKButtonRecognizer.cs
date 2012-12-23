c318143a709794d9a3dd26fe68dc801d6b990821
Mike <mike@prime31.com>
2012-12-22 17:17:46 -0800

    button recognizer added and made totally generic so it can be used with any sprite solution

---
+++ Assets/Plugins/GestureKit/Recognizers/GKButtonRecognizer.cs
@@ -0,0 +1,135 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class GKButtonRecognizer : GKAbstractGestureRecognizer
+{
+	private enum GKButtonState
+	{
+		Highlighted
+	}
+	
+	public event Action<GKButtonRecognizer> onSelectedEvent;
+	public event Action<GKButtonRecognizer> onDeselectedEvent;
+	public event Action<GKButtonRecognizer> onTouchUpInsideEvent;
+	
+	private Rect _defaultFrame;
+	private Rect _highlightedFrame;
+	
+	
+	/* State Definitions
+	 * Began: a touch started out on the button but has moved off. We still track it in case it comes back on the button
+	 * RecognizedAndStillRecognizing: a touch is currently down on the button
+	 * Recognized: 
+	 */
+	
+	#region constructors
+	
+	/// <summary>
+	/// the contstructors ensure we have a frame to work with for button recognizers
+	/// </summary>
+	public GKButtonRecognizer( Rect defaultFrame ) : this( defaultFrame, new RectOffset() )
+	{}
+	
+	public GKButtonRecognizer( Rect defaultFrame, RectOffset highlightedFrameOffsets )
+	{
+		_defaultFrame = defaultFrame;
+		_highlightedFrame = highlightedFrameOffsets.Add( defaultFrame );
+		boundaryFrame = _defaultFrame;
+	}
+	
+	#endregion
+	
+	
+	#region Button methods. Subclasses can override these or you can skip subclassing and just listen to the events
+	
+	/// <summary>
+	/// called when a touch has began on the button or reentered the frame
+	/// </summary>
+	protected virtual void onSelected()
+	{
+		// while selected, we use a highlighted frame to allow the touch to move a bit and still remain selected
+		boundaryFrame = _highlightedFrame;
+		
+		if( onSelectedEvent != null )
+			onSelectedEvent( this );
+	}
+	
+	
+	/// <summary>
+	/// called when a touch ends (if the button was already highlighted) or if a tracked touch exists the highlighted frame
+	/// </summary>
+	protected virtual void onDeselected()
+	{
+		if( onDeselectedEvent != null )
+			onDeselectedEvent( this );
+	}
+	
+	
+	/// <summary>
+	/// called if a tracked touch ends while the button is highlighted
+	/// </summary>
+	protected virtual void onTouchUpInside()
+	{
+		if( onTouchUpInsideEvent != null )
+			onTouchUpInsideEvent( this );
+	}
+	
+	#endregion
+	
+	
+	#region GKAbstractGestureRecognizer
+	
+	// we do nothing here. all event will be handled internally
+	internal override void fireRecognizedEvent() {}
+	
+	
+	internal override void touchesBegan( List<GKTouch> touches )
+	{
+		// grab the first touch that begins on us
+		if( state == GKGestureRecognizerState.Possible && touches[0].phase == TouchPhase.Began )
+		{
+			_trackingTouches.Add( touches[0] );
+			state = GKGestureRecognizerState.RecognizedAndStillRecognizing;
+			onSelected();
+		}
+	}
+	
+	
+	internal override void touchesMoved( List<GKTouch> touches )
+	{
+		// check to see if the touch is still in our frame
+		var isTouchInFrame = boundaryFrame.Value.Contains( touches[0].position );
+		
+		// if we are in the Began phase than we should switch to RecognizedAndStillRecognizing (highlighted) if the touch is in our frame
+		if( state == GKGestureRecognizerState.Began && isTouchInFrame )
+		{
+			state = GKGestureRecognizerState.RecognizedAndStillRecognizing;
+			onSelected();
+		}
+		else if( state == GKGestureRecognizerState.RecognizedAndStillRecognizing && !isTouchInFrame ) // if the touch exits the frame and we were highlighted deselect now
+		{
+			state = GKGestureRecognizerState.Began;
+			onDeselected();
+		}
+	}
+	
+	
+	internal override void touchesEnded( List<GKTouch> touches )
+	{
+		// if we were previously highlighted (RecognizedAndStillRecognizing) we have an official touch
+		if( state == GKGestureRecognizerState.RecognizedAndStillRecognizing )
+			onTouchUpInside();
+		
+		// reset the boundary frame
+		boundaryFrame = _defaultFrame;
+		
+		state = GKGestureRecognizerState.Failed;
+	}
+	
+	#endregion
+
+}
