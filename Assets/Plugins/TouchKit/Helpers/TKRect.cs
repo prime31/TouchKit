684c44c9a3102a63b0fd0531fedaadd2ee76c711
Mike <mike@prime31.com>
2014-01-26 00:15:15 -0700

    committing this just because its late and I’m really tired and i don’t know why it works

--- Assets/Plugins/TouchKit/Helpers/TKRect.cs
+++ Assets/Plugins/TouchKit/Helpers/TKRect.cs
@@ -4,11 +4,13 @@ using System.Collections;
 
 
 /// <summary>
-/// replacement for the Unity Rect class that is GestureKit and resolution-aware. Creating one while on a retina device will automatically double all values
-/// if GestureKit.autoUpdateRectsForRetina is true and GestureKit.isRetina is true.
+/// replacement for the Unity Rect class that is TouchKit and resolution-aware. Creating one while on a retina device will automatically double all values
+/// if TouchKit autoUpdateRects is true
 /// </summary>
 public struct TKRect
 {
+	public TKAnchor anchor;
+
 	public float x;
 	public float y;
 	public float width;
@@ -18,31 +20,110 @@ public struct TKRect
 	public float xMax { get { return x + width; } }
 	public float yMin { get { return y; } }
 	public float yMax { get { return y + height; } }
+	public Vector2 center { get { return new Vector2( x + ( width / 2f ), y + ( height / 2f ) ); } }
 	
 	
-	public TKRect( float x, float y, float width, float height )
+	public TKRect( float x, float y, float width, float height ) : this( x, y, width, height, TKAnchor.Center )
+	{}
+
+
+	public TKRect( float x, float y, float width, float height, TKAnchor anchor )
 	{
-		var multiplier = TouchKit.instance.retinaMultiplier;
-		
-		this.x = x * multiplier;
-		this.y = y * multiplier;
-		this.width = width * multiplier;
-		this.height = height * multiplier;
+		this.anchor = anchor;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		updateRectWithRuntimeScaleModifier();
+	}
+
+
+	private void updateRectWithRuntimeScaleModifier()
+	{
+		var multiplier = TouchKit.instance.runtimeScaleModifier;
+
+		var newWidth = width * multiplier.x;
+		var newHeight = height * multiplier.y;
+
+		Debug.Log( "multiplier: " + multiplier );
+		Debug.Log( "old center: " + center );
+		Debug.Log( "old y: " + y );
+		Debug.Log( "old yMAX: " + yMax );
+
+		// x and y vary based on our anchor
+		switch( anchor )
+		{
+			case TKAnchor.TopLeft:
+			case TKAnchor.MiddleLeft:
+			case TKAnchor.BottomLeft:
+				x *= multiplier.x;
+				break;
+
+			case TKAnchor.TopCenter:
+			case TKAnchor.Center:
+			case TKAnchor.BottomCenter:
+				x *= multiplier.x;
+				break;
+
+			case TKAnchor.TopRight:
+			case TKAnchor.MiddleRight:
+			case TKAnchor.BottomRight:
+				x *= multiplier.x;
+				break;
+		}
+
+		switch( anchor )
+		{
+			case TKAnchor.TopLeft:
+			case TKAnchor.TopCenter:
+			case TKAnchor.TopRight:
+				y *= multiplier.y;
+				break;
+			
+			case TKAnchor.MiddleLeft:
+			case TKAnchor.Center:
+			case TKAnchor.MiddleRight:
+				y *= multiplier.y;
+				break;
+
+			case TKAnchor.BottomLeft:
+			case TKAnchor.BottomCenter:
+			case TKAnchor.BottomRight:
+				y *= multiplier.y;
+				break;
+		}
+
+		// width and height are always just directly modified
+		width = newWidth;
+		height = newHeight;
+
+		Debug.Log( "new center: " + center );
+		Debug.Log( "screen center: " + Screen.width / 2f + ", " + Screen.height / 2f );
+		Debug.Log( "new y: " + y );
+		Debug.Log( "new yMAX: " + yMax );
 	}
 	
 	
 	public TKRect copyWithExpansion( float allSidesExpansion )
 	{
-		allSidesExpansion *= TouchKit.instance.retinaMultiplier;
 		return copyWithExpansion( allSidesExpansion, allSidesExpansion );
 	}
 	
 	
 	public TKRect copyWithExpansion( float xExpansion, float yExpansion )
 	{
-		xExpansion *= TouchKit.instance.retinaMultiplier;
-		yExpansion *= TouchKit.instance.retinaMultiplier;
-		return new TKRect( x - xExpansion, y - yExpansion, width + ( xExpansion + yExpansion ), height + ( xExpansion + yExpansion ) );
+		xExpansion *= TouchKit.instance.runtimeScaleModifier.x;
+		yExpansion *= TouchKit.instance.runtimeScaleModifier.y;
+
+		var rect = new TKRect();
+		rect.x = this.x - xExpansion;
+		rect.y = this.y - yExpansion;
+		rect.width = this.width + ( xExpansion * 2f );
+		rect.height = this.height + ( yExpansion * 2f );
+
+		return rect;
+		//TKRect( x - xExpansion, y - yExpansion, width + ( xExpansion + yExpansion ), height + ( xExpansion + yExpansion ) );
 	}
 	
 	
