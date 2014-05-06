f7b1e174626f7ea0380d41255d61769856fb9d02
fresswolf <andreas.siegrist@gmail.com>
2014-05-06 14:25:39 +0200

    applied P31 style policy

--- Assets/Plugins/TouchKit/Helpers/TKTouch.cs
+++ Assets/Plugins/TouchKit/Helpers/TKTouch.cs
@@ -1,116 +1,113 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class TKTouch
 {
-	public readonly int fingerId;
-	public Vector2 position;
-	public Vector2 deltaPosition;
-	public float deltaTime;
-	public int tapCount;
-	public TouchPhase phase = TouchPhase.Ended;
+		public readonly int fingerId;
+		public Vector2 position;
+		public Vector2 deltaPosition;
+		public float deltaTime;
+		public int tapCount;
+		public TouchPhase phase = TouchPhase.Ended;
 	
 	#if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_WEBPLAYER
-	// used to track mouse movement and fake touches
-	private Vector2? _lastPosition;
-	private double _lastClickTime;
-	private double _multipleClickInterval = 0.2;
+		// used to track mouse movement and fake touches
+		private Vector2? _lastPosition;
+		private double _lastClickTime;
+		private double _multipleClickInterval = 0.2;
 	#endif
 	
 	
-	public TKTouch(int fingerId)
-	{
-		// lock this TKTouch to the fingerId
-		this.fingerId = fingerId;
-	}
+		public TKTouch( int fingerId )
+		{
+				// lock this TKTouch to the fingerId
+				this.fingerId = fingerId;
+		}
 	
-	public TKTouch populateWithTouch(Touch touch)
-	{
-		position = touch.position;
-		deltaPosition = touch.deltaPosition;
-		deltaTime = touch.deltaTime;
-		tapCount = touch.tapCount;
-		phase = touch.phase;
+		public TKTouch populateWithTouch( Touch touch )
+		{
+				position = touch.position;
+				deltaPosition = touch.deltaPosition;
+				deltaTime = touch.deltaTime;
+				tapCount = touch.tapCount;
+				phase = touch.phase;
 		
-		return this;
-	}
-	
-	
+				return this;
+		}
+
 	#if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_WEBPLAYER
-	
-	
-	public TKTouch populateWithPosition(Vector3 currentPosition, TouchPhase touchPhase)
-	{
-		Vector2 currentPosition2d = new Vector2(currentPosition.x, currentPosition.y);
+		public TKTouch populateWithPosition( Vector3 currentPosition, TouchPhase touchPhase )
+		{
+				Vector2 currentPosition2d = new Vector2( currentPosition.x, currentPosition.y );
 		
-		// if we have a lastMousePosition use it to get a delta
-		if (_lastPosition.HasValue)
-		{
-			deltaPosition = currentPosition2d - _lastPosition.Value;
-		} else
-		{
-			deltaPosition = new Vector2(0, 0);
-		}
+				// if we have a lastMousePosition use it to get a delta
+				if( _lastPosition.HasValue )
+				{
+						deltaPosition = currentPosition2d - _lastPosition.Value;
+				} else
+				{
+						deltaPosition = new Vector2( 0, 0 );
+				}
 		
-		switch (touchPhase)
-		{
-		case TouchPhase.Began:
-			phase = TouchPhase.Began;
+				switch( touchPhase )
+				{
+						case TouchPhase.Began:
+								phase = TouchPhase.Began;
 			
-			// check for multiple clicks
-			if (Time.time < _lastClickTime + _multipleClickInterval)
-				tapCount++;
-			else
-				tapCount = 1;
-			_lastPosition = currentPosition2d;
-			_lastClickTime = Time.time;
-			break;
-		case TouchPhase.Stationary:
-		case TouchPhase.Moved:
-			if (deltaPosition.magnitude == 0)
-			{
-				phase = TouchPhase.Stationary;   
-			} else
-			{
-				phase = TouchPhase.Moved;  
-			}
-			_lastPosition = currentPosition2d;
-			break;
-		case TouchPhase.Ended:
-			phase = TouchPhase.Ended;
-			_lastPosition = null;
-			break;
+								// check for multiple clicks
+								if( Time.time < _lastClickTime + _multipleClickInterval )
+										tapCount++;
+								else
+										tapCount = 1;
+								_lastPosition = currentPosition2d;
+								_lastClickTime = Time.time;
+								break;
+						case TouchPhase.Stationary:
+						case TouchPhase.Moved:
+								if( deltaPosition.magnitude == 0 )
+								{
+										phase = TouchPhase.Stationary;   
+								} else
+								{
+										phase = TouchPhase.Moved;  
+								}
+								_lastPosition = currentPosition2d;
+								break;
+						case TouchPhase.Ended:
+								phase = TouchPhase.Ended;
+								_lastPosition = null;
+								break;
+				}
+		
+				position = currentPosition2d;
+		
+				return this;
 		}
-		
-		position = currentPosition2d;
-		
-		return this;
-	}
 	
 	
-	public TKTouch populateFromMouse()
-	{
-		// do we have some input to work with?
-		if (Input.GetMouseButtonUp(0) || Input.GetMouseButton(0))
+		public TKTouch populateFromMouse()
 		{
-			TouchPhase phase = TouchPhase.Moved;
-			if (Input.GetMouseButtonUp(0))
-				phase = TouchPhase.Ended;
-			if (Input.GetMouseButtonDown(0))
-				phase = TouchPhase.Began;
+				// do we have some input to work with?
+				if( Input.GetMouseButtonUp( 0 ) || Input.GetMouseButton( 0 ) )
+				{
+						TouchPhase phase = TouchPhase.Moved;
+						if( Input.GetMouseButtonUp( 0 ) )
+								phase = TouchPhase.Ended;
+						if( Input.GetMouseButtonDown( 0 ) )
+								phase = TouchPhase.Began;
 			
-			var currentMousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-			this.populateWithPosition(currentMousePosition, phase);
-		}
+						var currentMousePosition = new Vector2( Input.mousePosition.x, Input.mousePosition.y );
+						this.populateWithPosition( currentMousePosition, phase );
+				}
 		
-		return this;
-	}
+				return this;
+		}
 	#endif
 	
-	
-	public override string ToString()
-    {
-        return string.Format("[TKTouch] fingerId: {0}, phase: {1}, position: {2}", fingerId, phase, position);
-    }
+		public override string ToString()
+		{
+				return string.Format( "[TKTouch] fingerId: {0}, phase: {1}, position: {2}", fingerId, phase, position );
+		}
     
 }
