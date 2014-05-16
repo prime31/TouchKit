bb95d3d2d2db3fb67a37b2fb9bad3fb22849971a
Mike <mike@prime31.com>
2014-05-16 13:00:39 -0700

    added a new constructor that takes size and center

--- Assets/TouchKit/Helpers/TKRect.cs
+++ Assets/TouchKit/Helpers/TKRect.cs
@@ -4,8 +4,10 @@ using System.Collections;
 
 
 /// <summary>
-/// replacement for the Unity Rect class that is TouchKit and resolution-aware. Creating one while on a retina device will automatically double all values
-/// if TouchKit autoScaleRectsAndDistances is true
+/// replacement for the Unity Rect class that is TouchKit and resolution-aware. Creating one will automatically scale all values
+/// if TouchKit autoScaleRectsAndDistances is true based on your designTimeResolution.
+///
+/// Note the TKRects use the bottom-left as the origin.
 /// </summary>
 public struct TKRect
 {
@@ -13,13 +15,13 @@ public struct TKRect
 	public float y;
 	public float width;
 	public float height;
-	
+
 	public float xMin { get { return x; } }
 	public float xMax { get { return x + width; } }
 	public float yMin { get { return y; } }
 	public float yMax { get { return y + height; } }
 	public Vector2 center { get { return new Vector2( x + ( width / 2f ), y + ( height / 2f ) ); } }
-	
+
 
 	public TKRect( float x, float y, float width, float height )
 	{
@@ -27,6 +29,17 @@ public struct TKRect
 		this.y = y;
 		this.width = width;
 		this.height = height;
+
+		updateRectWithRuntimeScaleModifier();
+	}
+
+
+	public TKRect( float width, float height, Vector2 center )
+	{
+		this.width = width;
+		this.height = height;
+		this.x = center.x - width / 2f;
+		this.y = center.y - height / 2f;
 
 		updateRectWithRuntimeScaleModifier();
 	}
@@ -41,14 +54,14 @@ public struct TKRect
 		width = width * multiplier.x;
 		height = height * multiplier.y;
 	}
-	
-	
+
+
 	public TKRect copyWithExpansion( float allSidesExpansion )
 	{
 		return copyWithExpansion( allSidesExpansion, allSidesExpansion );
 	}
-	
-	
+
+
 	public TKRect copyWithExpansion( float xExpansion, float yExpansion )
 	{
 		xExpansion *= TouchKit.instance.runtimeScaleModifier.x;
@@ -62,19 +75,19 @@ public struct TKRect
 
 		return rect;
 	}
-	
-	
+
+
 	public bool contains( Vector2 point )
 	{
 		if( x <= point.x && y <= point.y && xMax >= point.x && yMax >= point.y )
 			return true;
 		return false;
 	}
-	
-	
+
+
 	public override string ToString()
 	{
 		return string.Format( "TKRect: x: {0}, xMax: {1}, y: {2}, yMax: {3}, width: {4}, height: {5}", x, xMax, y, yMax, width, height );
 	}
-	
+
 }
